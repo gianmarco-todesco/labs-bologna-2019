c5454e31b0d23f937d0feb15783540cc4fd445b5
gianmarco-todesco <gianmarco.todesco@gmail.com>
2019-06-10 19:28:03 +0200

    .

--- day3/fourier.js
+++ day3/fourier.js
@@ -21,52 +21,73 @@ function anima() {
 
 // disegna tutto
 function disegna() {
-    var t = performance.now() * 0.0001;
 
+    // tempo (in secondi) dall'inizio
+    var t = performance.now() * 0.001;
+
+    // centro del canvas
     var cx = canvas.width/2;
     var cy = canvas.height/2;
 
+    // parto dal centro del canvas : (x,y) = (cx,cy)
     var x= cx, y = cy;
-    var phi;
 
-    var r;
+    // phi0 è la rotazione del primo cerchio (la più lenta)
+    var phi0 = t * Math.PI * 2 / 10.0;
 
+    // inizializzo larghezza e colore della linea
     ctx.lineWidth = 1;
     ctx.strokeStyle = "black";
-
+    
+    // ciclo su tutti i coefficienti (l'array C)
     for(var i=0; i<C.length; i++) {
-        r = C[i];
+        // raggio
+        var r = C[i];
+        // disegno il cerchio i-esimo
         cerchio(x, y, r);
+        // calcolo la rotazione del cerchio i-esimo
+        // (il secondo cerchio è veloce il doppio,
+        // il terzo il triplo, ecc.)
+        var phi = (i+1) * phi0;
 
-        phi = (i+1) * t * Math.PI * 2;
-
+        // disegno la linea che congiunge i centri 
+        // dei cerchi i-esimo e (i+1)-esimo
         ctx.beginPath();
         ctx.moveTo(x,y);
+        // (x,y) diventa la posizione del cerchio (i+1)-esimo
         x = x + r*Math.cos(phi);
         y = y + r*Math.sin(phi);
         ctx.lineTo(x,y);
         ctx.stroke();
     }
 
+    // aggiungo la posizione del punto terminale all'array traccia[]
     traccia.push([x,y]);
 
+    // faccio in modo che traccia[] non superi mai la lunghezza di
+    // m elementi
+    var m = 500;
+    if(traccia.length>m)
+        traccia.splice(0,traccia.length-m);
+
+    // disegno la traccia
     if(traccia.length>=2) {
         ctx.beginPath();
         ctx.moveTo(traccia[0][0], traccia[0][1]);
         for(var j=1; j<traccia.length;j++) {
             ctx.lineTo(traccia[j][0], traccia[j][1]);
         }
+        // linea spessa, colore rosso
         ctx.lineWidth = 4;
-        ctx.strokeStyle = "#FF8888";
+        ctx.strokeStyle = "#FF6666";
         ctx.stroke();
-
     }
 
-
-
-
-
+    // disegno un puntino in corrispondenza del punto terminale
+    cerchio(x,y,3);
+    ctx.fill();
 }
+
 
 function cerchio(x,y,r) {
     ctx.beginPath();
